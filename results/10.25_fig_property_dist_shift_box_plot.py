c32600370d503eac36b29717c28736a530528021
KimHyeonsu <kim1231232@kaist.ac.kr>
2022-11-24 12:01:30 +0900

    results

--- results/10.25_fig_property_dist_shift_box_plot.py
+++ results/10.25_fig_property_dist_shift_box_plot.py
@@ -21,11 +21,13 @@ est_c = 0.4
 
 smiles_list = []
 
+total_num = 10
+interval = 10
 
-homo_list = [[] for i in range(5)]
-lumo_list = [[] for i in range(5)]
-s1_list = [[] for i in range(5)]
-est_list = [[] for i in range(5)]
+homo_list = [[] for i in range(total_num)]
+lumo_list = [[] for i in range(total_num)]
+s1_list = [[] for i in range(total_num)]
+est_list = [[] for i in range(total_num)]
 
 def obtain_mean_variance(vals):
     mean = sum(vals) / len(vals)
@@ -50,41 +52,39 @@ def read_data(filename):
         pub_st += [s1-t1]
     total = len(pub_lines)
     
-    pub_ps =  [obtain_mean_variance(ps) for ps in [pub_homo,pub_lumo, pub_s1, pub_st] ]
+    pub_ps =  [ps for ps in [pub_homo,pub_lumo, pub_s1, pub_st] ]
     return pub_ps
 
-tadf_ps = read_data('../data/total_train_QC_data.txt')
-
+tadf_ps = read_data('./total_train_QC_data.txt')
 
 for fn in fns:
     with open(fn) as f:
         lines = f.readlines()
     for line in lines:
         homo, lumo, s1, t1, f, score = [float(val) for val in line.split()[2:]]
-        #homo = abs(homo-tadf_ps[0][0])
-        #lumo = abs(lumo-tadf_ps[1][0])
-        #est = abs(s1-t1 - tadf_ps[3][0])
-        #s1 = abs(s1-tadf_ps[2][0])
         est = s1-t1
-        if est < 0:
+        i = int(score/interval)
+        if  i == 9 and est > 0.4:
             print(line)
-        i = int(score/10)-5
+
+
         homo_list[i].append(homo)
         lumo_list[i].append(lumo)
         s1_list[i].append(s1)
         est_list[i].append(est)
 
+#sys.exit()
 colors = ['y','orange','r','purple','black']
 
 
-homo_list =  [homo_list[i]  for i in range(5)]
-lumo_list =  [lumo_list[i]  for i in range(5)]
-s1_list =  [s1_list[i]  for i in range(5)]
-est_list = [est_list[i]  for i in range(5)]
+homo_list =  [homo_list[i]  for i in range(total_num)]
+lumo_list =  [lumo_list[i]  for i in range(total_num)]
+s1_list =  [s1_list[i]  for i in range(total_num)]
+est_list = [est_list[i]  for i in range(total_num)]
 
 
 properties = [homo_list, lumo_list, s1_list, est_list]
-print(properties)
+
 import matplotlib
 from matplotlib.ticker import MultipleLocator
 import matplotlib.ticker as ticker
@@ -97,46 +97,25 @@ legend_fontsize = 16
 
 
 
-
-values = ['50-60','60-70','70-80','80-90','90-100']
+values =  [f'{50+int(i*interval)}-{50+int((i+1)*interval)}' for i in range(5)] +['TADF']
 labels = ['HOMO (eV)', 'LUMO (eV)', '$E(S_{1})$', '$\Delta E_{ST}$']
 colors = ['y','orange','r','purple','black']
-ylim_list = [[-7.7,-1.9], [-3.0,1.65], [0.5, 6.5], [-.12, 2.5] ]
+ylim_list = [[-7.7,-1.9], [-3.0,1.65], [0.5, 6.5], [-.06, 2.2] ]
 
 fig = plt.figure(figsize=(15,12))
 
 for i in range(4):
     ax =plt.subplot(2,2,i+1)
-    #sample_props = []
-    sample_props = properties[i]
-    #variances = []
-    #for j in range(5):
-    #    sample_props.append(abs(properties[i][j][0]-tadf_ps[i][0]))
-    #    variances.append(properties[i][j][1])
-
-    #s = [80  for k in range(len(variances))]
-    #plt.errorbar(values, sample_props, yerr=variances, fmt="o", marker='^', lw=2, color=colors[i],capsize=5, capthick=2)   
-    #plt.scatter(values, sample_props, marker='^', color=colors[i], label=labels[i], s= s)
-    #data = [np.random.normal(0, std, 1000) for std in range(1, 6)]
+    sample_props = properties[i][5:]+[tadf_ps[i][5:]]
 
     box = plt.boxplot(sample_props, notch=False, patch_artist=True, labels=values)
-    colors = ['cyan', 'lightblue', 'lightgreen', 'tan', 'pink']
+    colors = ['cyan', 'lightblue', 'lightgreen', 'tan', 'pink','ivory']
     for patch, color in zip(box['boxes'], colors):
         patch.set_facecolor(color)
-    plt.plot([1,5],[tadf_ps[i][0] for k in range(2)])
-    #plt.bar(values, sample_props, color=colors[i], label=labels[i])
     plt.xlabel('TADF-likeness', fontsize=label_fontsize)
     plt.ylabel(f'{labels[i]}', fontsize=label_fontsize, color='k')
     plt.tick_params(length=tick_length, width=tick_width, labelsize=tick_labelsize, labelcolor='k', color='k')
     plt.ylim(ylim_list[i])
     ax.yaxis.set_major_locator(ticker.LinearLocator(5)) 
-    #matplotlib.rcParams['ytick.major.pad'] = 3
-    #y_interval = round((max(sample_props)-min(sample_props))/5,1)
-    #y_ticks = [ y_interval*k+round(min(sample_props),1) for k in range(1,5)]
-    #plt.yticks(y_ticks)
-    #plt.legend(fontsize=legend_fontsize)
     plt.tight_layout()
-    #plt.rc('font', size=10)
-#label = 'MAE of eigenvalues (eV)'
-#plt.subplots(constrained_layout=True)
 plt.show()
--- results/plt_score_simil_cutoff.py
+++ results/plt_score_simil_cutoff.py
@@ -10,11 +10,12 @@ from multiprocessing import Pool
 import pandas as pd
 import matplotlib
 from matplotlib.ticker import MultipleLocator
-from sklearn.cluster import KMeans
 from sklearn.preprocessing import StandardScaler
 from sklearn.pipeline import make_pipeline
 from sklearn.preprocessing import MinMaxScaler
 from matplotlib.patches import Rectangle
+
+sns.set()
 
 y_scaler1 = MinMaxScaler()
 
@@ -24,10 +25,7 @@ tick_width = 1.5
 tick_labelsize = 16
 legend_fontsize = 16
 
-plt.style.use('ggplot')
-fig, ax = plt.subplots(figsize=(6.8,5.1))
-#fig, ax = plt.subplots(figsize=(7,5))
-#fig = plt.figure(figsize=(10,6))
+fig = plt.figure(figsize=(7,5))
 
 def multiprocessing(function,elements, ncores):
     pool = Pool(processes = ncores)
@@ -58,24 +56,19 @@ def cal_max_sim(smiles):
             max_sim = sim
             nearest_smiles = smiles_list[i]
         
+    mean_sim=np.mean(np.array(sim_list))
     return max_sim, nearest_smiles
 
 #label_ind  = 0
 import pandas as pd
 import seaborn as sns
 #fns = list(glob.iglob('pubchem_sampling_*txt'))
-#fns = ['TADF-likeness-unseen_10.31.txt']
-
 fns = ['TADF-likeness-unseen_no_transfer.txt']
-
-
-
+#fns = ['TADF-likeness-unseen_old.txt']
+#fns = ['TADF-likeness-pubchem_no_transfer.txt']
 #pipeline = make_pipeline(scaler,model)
 #scaler = StandardScaler()
-k = 5
-model = KMeans(n_clusters=k)
-likeness_scaler = MinMaxScaler()
-ratio = 0.5
+ratio = 0.8
 
 for fn in fns:
     
@@ -112,72 +105,30 @@ for fn in fns:
         sort_nearest_smiles_list.append(nearest_smiles)
         ind_list.append(ind)
     cutoff_sim = sort_sim_list[int(len(sort_sim_list)*(1-ratio))]
-    likeness_scaler.fit_transform(np.array(likeness_list).reshape(-1,1))
-    scaled_likeness_list = likeness_scaler.transform(np.array(likeness_list).reshape(-1,1))
-    X = []
-    for sim, likeness in zip(sort_sim_list, scaled_likeness_list):
-        X.append(np.array([sim,likeness]))
-    X = np.array(X)
-    model.fit(X)
-    label = model.labels_
-    likeness_list_ = np.array(likeness_list)
+    
+    tmp = likeness_list[:]
+    tmp.sort()
+    cutoff_likeness = tmp[int(len(tmp)*(1-ratio))]
 
-    #plt.plot([cutoff_likeness, cutoff_likeness], [0.2, 1.05])
-    X = np.transpose(X)
-    for ind, smiles, sim, likeness, nearest_smiles in zip(ind_list, sort_smiles_list, sort_sim_list, likeness_list, sort_nearest_smiles_list):
-        print(ind, smiles, sim, likeness, nearest_smiles)    
-    likeness_list.sort()
-    cutoff_likeness = likeness_list[int(len(likeness_list)*(1-ratio))]
+    upper_likeness_list = []
+    upper_sim_list = []
+    for likeness, sim in zip(likeness_list, sort_sim_list):
+        if likeness > cutoff_likeness:
+            upper_sim_list.append(sim)
+            upper_likeness_list.append(likeness)
+
     colors = ['violet','silver','springgreen','gold','deepskyblue']
-    for i in range(k):
-        plt.scatter(likeness_list_[label == i] , X[0][label == i] , label = i,  linewidths=0.5, edgecolors='k', c=colors[4])
-    plt.xlabel(rf'TADF-likeness (TL)', fontsize=label_fontsize, color='k')
-    plt.ylabel('Structural similarity (SS)', fontsize=label_fontsize, color='k')
-    plt.tick_params(length=tick_length, width=tick_width, labelsize=tick_labelsize, labelcolor='k', color='k')
-    #plt.plot([60, 100], [cutoff_sim, cutoff_sim])
-    print('cutoff 1. sim 2.likeness', cutoff_sim, cutoff_likeness)
-    ax.add_patch(Rectangle((cutoff_likeness, 0.2), 100.1-cutoff_likeness, 0.9,alpha=0.3, color= 'lime'))
-    ax.add_patch(Rectangle((60, cutoff_sim), 50, 1.05-cutoff_sim,alpha=0.3, color= 'lightcoral'))
-    plt.text(61, cutoff_sim+0.01, "40 % <",fontsize=13, weight='bold')    
-    plt.text(cutoff_likeness+1, 0.22, "40 % <",fontsize=13, weight='bold')    
-    matplotlib.rcParams['ytick.major.pad'] = 3
-    plt.rc('font', size=40)
-    #plt.legend()
-    #plt.legend(prop={'size': 16}, loc='upper left', ncol=1)
+    x_start = 60
 
-    plt.ylim([0.2, 1.05])
-    plt.xlim([60, 100.1])
-    plt.yticks([0.2,0.4,0.6,0.8,1.0])
-    plt.xticks([60,70,80,90,100])
-    plt.tight_layout()
+    #plt.ylim([0.2, 1.05])
+    #plt.xlim([x_start, 100.1])
+    #plt.yticks([0.2,0.4,0.6,0.8,1.0])
+    #sns.kdeplot(upper_sim_list)
+    sns.histplot(upper_sim_list, color='springgreen' ,bins=7, shrink=.97 )
+    plt.tick_params(length=tick_length, width=tick_width, labelsize=tick_labelsize, labelcolor='k', color='k')
+    plt.xlabel(rf'Structral similarity (SS)', fontsize=label_fontsize, color='k')
+    plt.ylabel('Count', fontsize=label_fontsize, color='k')
 
     plt.show()
 
 
-
-    if False:
-        fn = 'TADF-likeness-chromophore.txt'
-        with open(fn) as f:
-            lines = f.readlines()
-        smiles_candidates = []
-        #mean_TADF_likeness = sum(likeness_list)/len(likeness_list)
-        #mean_sim = sum(sort_sim_list)/len(sort_sim_list)
-        #print('TADF-likeness(mean)', mean_TADF_likeness)
-        #chromophore_likeness_ref = []
-        #for line in lines:
-        #    smiles = line.strip().split()[1]
-        #    smiles_candidates.append(smiles)
-        #    likeness = float(line.strip().split()[2])
-        #    chromophore_likeness_ref.append(likeness)
-
-        #mean_likeness = sum(chromophore_likeness_ref)/len(chromophore_likeness_ref)
-        #median_likeness = np.median(chromophore_likeness_ref)
-
-        pub_sim_list = list(multiprocessing(cal_max_sim, smiles_candidates, 8))
-        pub_mean = sum(pub_sim_list)/len(pub_sim_list)
-        pub_median = np.median(pub_sim_list)
-        
-        plt.plot([70, 100],[pub_mean, pub_mean])
-        plt.plot([mean_likeness,mean_likeness], [0.2,1.1])
-        plt.plot([mean_TADF_likeness,mean_TADF_likeness], [0.2,1.1])
-        print('pub_mean : ', pub_mean, len(pub_sim_list), mean_likeness, pub_median, median_likeness)
